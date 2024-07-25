ebb32b88d3ff70dce5116a7ee57f3de1dcecdb7e
shodon2007 <asrorovirbis@gmail.com>
2024-07-25 09:31:10 +0300

    Added start for Redis

---
+++ service/redis-service.js
@@ -0,0 +1,26 @@
+const redis = require('redis')
+
+class Redis {
+  constructor (this) {
+    this.client = redis.createClient()
+    this.client.on("error", err => console.error("Error in Redis ->", err))
+  }
+  
+  async connect() {
+    await this.client.connect()
+  }
+
+  async setTTL(key, data) {
+    await this.client.setEx(key, "3600", data)
+  }
+
+  async get(key) {
+    await this.client.get(key)
+  }
+
+  async set(key, data) {
+    await this.client.set(key, data)
+  }
+
+  
+}
